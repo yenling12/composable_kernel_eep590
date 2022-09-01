4312bed499bac460f6ab5d7abc4baddd95249214
shaojiewang <wsjmessi@163.com>
2022-09-01 13:20:07 +0800

    add example code for splitk bias

---
+++ example/42_splitK_gemm_bias/splitK_gemm_bias_xdl_fp16.cpp
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018-2022, Advanced Micro Devices, Inc. All rights reserved.
+
+#include "splitK_gemm_bias_xdl_fp16.hpp"
+
---
+++ example/42_splitK_gemm_bias/splitK_gemm_bias_xdl_fp16.hpp
@@ -0,0 +1,2 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018-2022, Advanced Micro Devices, Inc. All rights reserved.
