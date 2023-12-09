8174e87fbd00386bf1ca8ffa77c04032ac1cedb9
Junhao Zhang <howiejayzh@gmail.com>
2023-12-09 19:10:15 +0800

    Move NumHeads_KV from template arguments to normal arguments

--- example/32_batched_gemm_scale_softmax_gemm/grouped_query_attention_forward_wmma_fp16.cpp
+++ example/32_batched_gemm_scale_softmax_gemm/grouped_query_attention_forward_wmma_fp16.cpp
@@ -3,71 +3,76 @@
 
 /*
 Grouped Query Attention,
-Ainslie, Joshua, James Lee-Thorp, Michiel de Jong, Yury Zemlyanskiy, Federico Lebrón, and Sumit
-Sanghai. “GQA: Training Generalized Multi-Query Transformer Models from Multi-Head Checkpoints.”
-arXiv, May 22, 2023. https://doi.org/10.48550/arXiv.2305.13245.
+Ainslie, Joshua, James Lee-Thorp, Michiel de Jong, Yury Zemlyanskiy, Federico
+Lebrón, and Sumit Sanghai. “GQA: Training Generalized Multi-Query Transformer
+Models from Multi-Head Checkpoints.” arXiv, May 22, 2023.
+https://doi.org/10.48550/arXiv.2305.13245.
 
 Example is GQA-4
 */
 
+#include <cstdlib>
+#include <initializer_list>
 #include <iostream>
 #include <numeric>
-#include <initializer_list>
-#include <cstdlib>
 
 #include "ck/ck.hpp"
 #include "ck/tensor_operation/gpu/device/gemm_specialization.hpp"
-#include "ck/tensor_operation/gpu/device/tensor_specialization.hpp"
 #include "ck/tensor_operation/gpu/device/impl/device_grouped_query_attention_forward_wmma.hpp"
+#include "ck/tensor_operation/gpu/device/tensor_specialization.hpp"
 #include "ck/tensor_operation/gpu/element/element_wise_operation.hpp"
 
+#include "ck/library/reference_tensor_operation/cpu/reference_batched_gemm.hpp"
+#include "ck/library/reference_tensor_operation/cpu/reference_softmax.hpp"
 #include "ck/library/utility/check_err.hpp"
 #include "ck/library/utility/device_memory.hpp"
 #include "ck/library/utility/host_tensor.hpp"
 #include "ck/library/utility/host_tensor_generator.hpp"
 #include "ck/library/utility/literals.hpp"
-#include "ck/library/reference_tensor_operation/cpu/reference_batched_gemm.hpp"
-#include "ck/library/reference_tensor_operation/cpu/reference_softmax.hpp"
 
-template <ck::index_t... Is>
-using S = ck::Sequence<Is...>;
+template <ck::index_t... Is> using S = ck::Sequence<Is...>;
 
 using F16 = ck::half_t;
 using F32 = float;
 
 using PassThrough = ck::tensor_operation::element_wise::PassThrough;
 
-using ADataType        = F16;
-using B0DataType       = F16;
-using B1DataType       = F16;
-using Acc0DataType     = F32;
-using Acc1DataType     = F32;
+using ADataType = F16;
+using B0DataType = F16;
+using B1DataType = F16;
+using Acc0DataType = F32;
+using Acc1DataType = F32;
 using CShuffleDataType = F32;
-using CDataType        = F16;
+using CDataType = F16;
 using Acc0BiasDataType = ck::Tuple<>;
 using Acc1BiasDataType = ck::Tuple<>;
 
-static constexpr ck::index_t NumDimG          = 2;
-static constexpr ck::index_t NumDimM          = 1;
-static constexpr ck::index_t NumDimN          = 1;
-static constexpr ck::index_t NumDimK          = 1;
-static constexpr ck::index_t NumDimO          = 1;
+static constexpr ck::index_t NumDimG = 2;
+static constexpr ck::index_t NumDimM = 1;
+static constexpr ck::index_t NumDimN = 1;
+static constexpr ck::index_t NumDimK = 1;
+static constexpr ck::index_t NumDimO = 1;
 static constexpr ck::index_t QueryGroupNumber = 4;
 
-using AElementOp    = PassThrough;
-using B0ElementOp   = PassThrough;
+using AElementOp = PassThrough;
+using B0ElementOp = PassThrough;
 using Acc0ElementOp = ck::tensor_operation::element_wise::Scale;
-using B1ElementOp   = PassThrough;
-using CElementOp    = PassThrough;
+using B1ElementOp = PassThrough;
+using CElementOp = PassThrough;
 
-static constexpr auto GemmSpec = ck::tensor_operation::device::GemmSpecialization::MNKOPadding;
+static constexpr auto GemmSpec =
+    ck::tensor_operation::device::GemmSpecialization::MNKOPadding;
 static constexpr auto MaskingSpec =
     ck::tensor_operation::device::MaskingSpecialization::MaskDisabled;
 
-static constexpr auto TensorSpecA  = ck::tensor_operation::device::TensorSpecialization::Default;
-static constexpr auto TensorSpecB0 = ck::tensor_operation::device::TensorSpecialization::Default;
-static constexpr auto TensorSpecB1 = ck::tensor_operation::device::TensorSpecialization::Default;
-static constexpr auto TensorSpecC  = ck::tensor_operation::device::TensorSpecialization::Default;
+static constexpr auto TensorSpecA =
+    ck::tensor_operation::device::TensorSpecialization::Default;
+static constexpr auto TensorSpecB0 =
+    ck::tensor_operation::device::TensorSpecialization::Default;
+static constexpr auto TensorSpecB1 =
+    ck::tensor_operation::device::TensorSpecialization::Default;
+static constexpr auto TensorSpecC =
+    ck::tensor_operation::device::TensorSpecialization::Default;
 
 // clang-format off
 // #define CK_MHA_USE_WAVE_1
@@ -83,7 +88,6 @@ using DeviceMHAFactory =
             ADataType, B0DataType, B1DataType, CDataType, Acc0BiasDataType, Acc0DataType, Acc1BiasDataType, Acc1DataType, CShuffleDataType,
             AElementOp, B0ElementOp, Acc0ElementOp, B1ElementOp, CElementOp,
             GemmSpec, TensorSpecA, TensorSpecB0, TensorSpecB1, TensorSpecC, 1,
-            QueryGroupNumber,
             32,
             //      Gemm 0
             16, 128, 64, 8,  8,
@@ -106,7 +110,6 @@ using DeviceMHAFactory =
             ADataType, B0DataType, B1DataType, CDataType, Acc0BiasDataType, Acc0DataType, Acc1BiasDataType, Acc1DataType, CShuffleDataType,
             AElementOp, B0ElementOp, Acc0ElementOp, B1ElementOp, CElementOp,
             GemmSpec, TensorSpecA, TensorSpecB0, TensorSpecB1, TensorSpecC, 1,
-            QueryGroupNumber,
             32,
             //      Gemm 0
             16, 64, 64, 8,  8,
@@ -131,7 +134,6 @@ using DeviceMHAFactory =
             ADataType, B0DataType, B1DataType, CDataType, Acc0BiasDataType, Acc0DataType, Acc1BiasDataType, Acc1DataType, CShuffleDataType,
             AElementOp, B0ElementOp, Acc0ElementOp, B1ElementOp, CElementOp,
             GemmSpec, TensorSpecA, TensorSpecB0, TensorSpecB1, TensorSpecC, 1,
-            QueryGroupNumber,
             64,
             //      Gemm 0
             32, 128, 64, 8, 8,
@@ -154,7 +156,6 @@ using DeviceMHAFactory =
             ADataType, B0DataType, B1DataType, CDataType, Acc0BiasDataType, Acc0DataType, Acc1BiasDataType, Acc1DataType, CShuffleDataType,
             AElementOp, B0ElementOp, Acc0ElementOp, B1ElementOp, CElementOp,
             GemmSpec, TensorSpecA, TensorSpecB0, TensorSpecB1, TensorSpecC, 1,
-            QueryGroupNumber,
             64,
             //      Gemm 0
             32, 64, 64, 8, 8,
@@ -179,7 +180,6 @@ using DeviceMHAFactory =
             ADataType, B0DataType, B1DataType, CDataType, Acc0BiasDataType, Acc0DataType, Acc1BiasDataType, Acc1DataType, CShuffleDataType,
             AElementOp, B0ElementOp, Acc0ElementOp, B1ElementOp, CElementOp,
             GemmSpec, TensorSpecA, TensorSpecB0, TensorSpecB1, TensorSpecC, 1,
-            QueryGroupNumber,
             128,
             //      Gemm 0
             64, 128, 64, 8, 8,
@@ -202,7 +202,6 @@ using DeviceMHAFactory =
             ADataType, B0DataType, B1DataType, CDataType, Acc0BiasDataType, Acc0DataType, Acc1BiasDataType, Acc1DataType, CShuffleDataType,
             AElementOp, B0ElementOp, Acc0ElementOp, B1ElementOp, CElementOp,
             GemmSpec, TensorSpecA, TensorSpecB0, TensorSpecB1, TensorSpecC, 1,
-            QueryGroupNumber,
             128,
             //      Gemm 0
             64, 64, 64, 8, 8,
@@ -227,7 +226,6 @@ using DeviceMHAFactory =
             ADataType, B0DataType, B1DataType, CDataType, Acc0BiasDataType, Acc0DataType, Acc1BiasDataType, Acc1DataType, CShuffleDataType,
             AElementOp, B0ElementOp, Acc0ElementOp, B1ElementOp, CElementOp,
             GemmSpec, TensorSpecA, TensorSpecB0, TensorSpecB1, TensorSpecC, 1,
-            QueryGroupNumber,
             256,
             //      Gemm 0
             128, 128, 64, 8, 8,   
@@ -250,7 +248,6 @@ using DeviceMHAFactory =
             ADataType, B0DataType, B1DataType, CDataType, Acc0BiasDataType, Acc0DataType, Acc1BiasDataType, Acc1DataType, CShuffleDataType,
             AElementOp, B0ElementOp, Acc0ElementOp, B1ElementOp, CElementOp,
             GemmSpec, TensorSpecA, TensorSpecB0, TensorSpecB1, TensorSpecC, 1,
-            QueryGroupNumber,
             256,
             //      Gemm 0
             128, 128, 64, 8, 8,   
@@ -273,30 +270,21 @@ using DeviceMHAFactory =
 // clang-format on
 // Ref Gemm0: fp16 in, fp32 out
 using ReferenceGemm0Instance =
-    ck::tensor_operation::host::ReferenceBatchedGemm_GQA<ADataType,
-                                                         B0DataType,
-                                                         Acc0DataType,
-                                                         Acc1DataType,
-                                                         AElementOp,
-                                                         B0ElementOp,
-                                                         Acc0ElementOp,
-                                                         QueryGroupNumber>;
+    ck::tensor_operation::host::ReferenceBatchedGemm_GQA<
+        ADataType, B0DataType, Acc0DataType, Acc1DataType, AElementOp,
+        B0ElementOp, Acc0ElementOp, QueryGroupNumber>;
 
 // Ref Softmax: fp32 in, fp16 out
 using ReferenceSoftmaxInstance =
-    ck::tensor_operation::host::ReferenceSoftmax<Acc0DataType, ADataType, Acc0DataType>;
+    ck::tensor_operation::host::ReferenceSoftmax<Acc0DataType, ADataType,
+                                                 Acc0DataType>;
 
 // Ref Gemm1: fp16 in, fp16 out
 using ReferenceGemm1Instance =
-    ck::tensor_operation::host::ReferenceBatchedGemm_GQA<ADataType,
-                                                         B1DataType,
-                                                         CDataType,
-                                                         Acc1DataType,
-                                                         AElementOp,
-                                                         B1ElementOp,
-                                                         CElementOp,
-                                                         QueryGroupNumber>;
+    ck::tensor_operation::host::ReferenceBatchedGemm_GQA<
+        ADataType, B1DataType, CDataType, Acc1DataType, AElementOp, B1ElementOp,
+        CElementOp, QueryGroupNumber>;
 
 #include "run_grouped_query_attention_forward_wmma.inc"
 
-int main(int argc, char* argv[]) { return run(argc, argv); }
+int main(int argc, char *argv[]) { return run(argc, argv); }
