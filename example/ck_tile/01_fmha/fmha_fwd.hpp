83830bd151e2d88416e6cd264631fec3246e58a6
PoYen, Chen <PoYen.Chen@amd.com>
2024-05-13 09:43:40 +0000

    Use accum type for output tensor

--- example/ck_tile/01_fmha/fmha_fwd.hpp
+++ example/ck_tile/01_fmha/fmha_fwd.hpp
@@ -26,7 +26,7 @@ struct FmhaFwdTypeConfig<ck_tile::half_t>
     using SMPLComputeDataType = float; // data type for reduction, softmax
     using PDataType           = ck_tile::half_t; // data type for A matrix of second gemm
     using OaccDataType        = float;           // data type for second gemm accumulation
-    using ODataType           = ck_tile::half_t;
+    using ODataType           = float;
 };
 
 template <>
